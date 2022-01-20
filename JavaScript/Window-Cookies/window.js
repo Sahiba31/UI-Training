9272ff5546e4f79dc1f61598f76745e7d8e49051
Sahiba31 <sahibagoyal1999@gmail.com>
2022-01-20 13:36:56 +0530

    Adding JS window

---
+++ JavaScript/Window-Cookies/window.js
@@ -0,0 +1,48 @@
+document.getElementById("demo1").innerHTML = "Width is " + window.innerWidth +
+    ", " + "Height is " + window.innerHeight;
+
+document.getElementById("demo2").innerHTML = "Screen Width and Height is: " +
+    screen.availWidth + " " + screen.availHeight;
+
+document.getElementById("demo3").innerHTML = "About Location HREF: " + window.location.href;
+
+document.getElementById("demo4").innerHTML = "About Location Hostname: " + window.location.hostname;
+
+document.getElementById("demo5").innerHTML = "About Location Protocol: " + window.location.protocol;
+
+
+//COOKIES
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 30);
+        }
+    }
+}
